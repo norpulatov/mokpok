1a73ae708b098d6c643737cf890d9b7f0b1fdf32
norpulatov <101542393+norpulatov@users.noreply.github.com>
2026-04-08 20:03:56 +0500

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .start import router as start_router
-from .admin import router as admin_router
-from .movies import router as movies_router
-from .callback import router as callback_router
-
-routers = [start_router, admin_router, movies_router, callback_router]
